13f1f5f86ba7d24a7925e012555d4d68495ab902
avengerpb <datlequoc96@gmail.com>
2017-08-15 20:00:48 +0700

    Unpack

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
  #!/bin/bash
 
-snmf=$PREFIX/opt/snmf
-mkdir -p $PREFIX/bin
-cp -r temp/*  $snmf
+snmf=$PREFIX/opt/snmf/
+mkdir -p $snmf
+mkdir -p $PREFIX/bin/
+cp -rf ./* $snmf/
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $snmf/Snmf.pl
 rm -f $snmf/Snmf.bak
-unzip $snmf/*.zip
-mv sNMF_CL_v1.2.zip/* $snmf/
-rm -rf temp
 chmod +x $snmf/snmf.sh
 chmod +x $snmf/plink
-chmod +x $snmf/install.command
-bash $snmf/install.command
 chmod 777 $snmf/Snmf.pl
 cp $snmf/snmf.sh $PREFIX/bin/
