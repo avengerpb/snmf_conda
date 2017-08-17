697ba552e966166fcc9760f2b9c99317a0623701
avengerpb <datlequoc96@gmail.com>
2017-08-17 16:11:40 +0700

    add lib

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
  #!/bin/bash
 
 snmf=$PREFIX/opt/snmf/
+chmod 777 $PREFIX/opt/snmf/
 mkdir -p $snmf
 mkdir -p $PREFIX/bin/
 cp -rf ./* $snmf/
+cp  $ssnmf/*.pm ../../lib/perl5/5.22.0/
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $snmf/Snmf.pl
 rm -f $snmf/Snmf.bak
 chmod +x $snmf/snmf.sh
 chmod +x $snmf/plink
-chmod 777 $snmf/Snmf.pl
-cp $snmf/snmf.sh $PREFIX/bin/
+chmod +x $snmf/Snmf.pl
+ln -s $snmf/snmf.sh $PREFIX/bin/
